2dc9386b9b0012f4303c47f18c29d2f691bab3b8
ryancontento <rkc61595@gmail.com>
2022-11-27 15:31:36 -0500

    Basic scraping of homepage

--- scraper.py
+++ scraper.py
@@ -1,6 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+parsed = BeautifulSoup(res.text, 'html.parser')
+parsed2 = BeautifulSoup(res2.text, 'html.parser')
 
+links = parsed.select('.titleline > a') 
+subtext = parsed.select('.subtext')
+links2 = parsed2.select('.titleline > a') 
+subtext2 = parsed2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+def sort_stories_by_votes(hnlist):
+  return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+
+def create_custom_hn(links, subtext):
+  hn = []
+  for idx, item in enumerate(links):
+    title = item.getText()
+    href = item.get('href', None)
+    vote = subtext[idx].select('.score')
+    if len(vote):
+      points = int(vote[0].getText().replace(' points', ''))
+      if points > 99:
+        hn.append({'title': title, 'link': href, 'votes': points})
+  return sort_stories_by_votes(hn)
+ 
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
